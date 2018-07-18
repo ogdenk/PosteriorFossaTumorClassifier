9b26e32b0776d12c66f0e7176448c6819c8a41f5
Kent Ogden <ogdenk@upstate.edu>
2018-07-18 16:32:13 -0400

    Initial commit of test code from geriatric epilepsy project

--- PosteriorFossaTumorClassifier.py
+++ PosteriorFossaTumorClassifier.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 trainFrac = 0.8 #Training fraction (includes validation)
 valFrac = 0.2 #Fraction of training data used for validation
 
-rootDir = 'd:/'
+rootDir = 'x:/'
 filename = 'rawavg.csv'
 datafile = os.path.join(rootDir, filename)
 
@@ -37,30 +37,34 @@ test_data = (test_data-means)/sigmas
 
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(train_data.shape[1],)))
+#model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
-history = model.fit(train_data,train_labels, epochs=30, batch_size=64, validation_data=(test_data, test_labels))
+history = model.fit(train_data,train_labels, epochs=40, batch_size=16, validation_data=(test_data, test_labels))
 history_dict = history.history
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
 epochs = range(1, len(loss_values) + 1)
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
-plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
-plt.title('Training and validation loss')
+
+
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+acc_values = history_dict['acc']
+plt.plot(epochs, acc_values, 'bo', label='Training acc')
+plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
 plt.clf()
-acc_values = history_dict['acc']
-val_acc_values = history_dict['val_acc']
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation accuracy')
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
